f6fdbb2f7e637b93f4dcdbf4ad1050467da1cb7b
Kristian Niemi <kristian.niemi@gmail.com>
2026-03-30 10:15:37 +0200

    (imports) Update deprecated DeviceInfo import path
    L-3: Change from homeassistant.helpers.entity to
    homeassistant.helpers.device_registry in sensor.py, binary_sensor.py,
    and calendar.py.

--- custom_components/karlstadsenergi/binary_sensor.py
+++ custom_components/karlstadsenergi/binary_sensor.py
@@ -7,7 +7,7 @@ from typing import Any
 
 from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity import DeviceInfo
+from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util import dt as dt_util
